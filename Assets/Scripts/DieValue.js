1dc92ddcbfe918224e6d7909bdd89c86fe717329
leopaglia <paglialungalf@gmail.com>
2015-02-06 01:16:53 -0300

    dadito re loco re mamado funcionando

---
+++ Assets/Scripts/DieValue.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+public var currentValue = 0;
+
+function Update() {
+
+//Debug.LogError(currentValue);
+
+//var dieTextGameObject = GameObject.Find("DieText");
+
+//var textMeshComponent = dieTextGameObject.GetComponent("Text");
+
+//textMeshComponent.guiText.text = currentValue.ToString();
+
+}
---
+++ Assets/Scripts/SideTrigger.js
@@ -0,0 +1,13 @@
+﻿public var faceValue = 0;
+
+function OnTriggerEnter( other : Collider ) {
+
+dieGameObject = GameObject.Find("SixSidedDie");
+
+dieValueComponent = dieGameObject.GetComponent("DieValue");
+
+dieValueComponent.currentValue = 7 - faceValue;
+
+Debug.LogError(dieValueComponent.currentValue);
+
+}
